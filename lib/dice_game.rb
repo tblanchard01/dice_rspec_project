16e5973837294bb95a63218b8f1ce122a2ab83d5
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-11 12:48:20 +0100

    added first rspec test which tests for the existance of the Dice object

---
+++ lib/dice_game.rb
@@ -0,0 +1,6 @@
+class Dice
+    def initialize
+        @numbers = [1,2,3,4,5,6]
+    end
+end
+ 
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,10 +4,18 @@ require 'dice_game'
 #So that I can play the game
 #I can have a dice
 # - dice is a class
-As a board game player,
-So that I know how many steps I should move
-I can roll a dice and get a number between 1 and 6
-As a dice app developper,
-So that I give players a good game experience
-I want the dice roll to be randomly picked
+#As a board game player,
+#So that I know how many steps I should move
+#I can roll a dice and get a number between 1 and 6
+#As a dice app developper,
+#So that I give players a good game experience
+#I want the dice roll to be randomly picked
+
+describe Dice do
+  describe "creates a new dice" do   
+    it "creates new dice object" do
+      expect(Dice.new).to be_instance_of Dice 
+    end
+ end 
+end 
 
