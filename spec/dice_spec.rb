53c77c0e645fee301c302630f0a8bbffdd0185f5
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-11 12:17:51 +0100

    added user stories to spec file

---
+++ spec/dice_spec.rb
@@ -0,0 +1,13 @@
+require 'dice_game'
+
+#As a board game player,
+#So that I can play the game
+#I can have a dice
+# - dice is a class
+As a board game player,
+So that I know how many steps I should move
+I can roll a dice and get a number between 1 and 6
+As a dice app developper,
+So that I give players a good game experience
+I want the dice roll to be randomly picked
+
